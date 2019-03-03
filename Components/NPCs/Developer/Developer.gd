7d1376dd1dc68b3f65a92f31c76b1f2858340de9
Matthew Barber <quitesimplymatt@gmail.com>
2019-03-03 15:03:31 +0000

    flipped position of dialoganchor respective to sprite

--- Components/NPCs/Developer/Developer.gd
+++ Components/NPCs/Developer/Developer.gd
@@ -1,9 +1,25 @@
 extends AnimatedSprite
 
+onready var DialogAnchor = $DialogAnchor
+var original_anchor_y
+var anchor_absolute_x
+
 const period = 4 # in s
 const amplitude = 8 # in something
 var time_elapsed = 0
 
+func _ready():
+	original_anchor_y = DialogAnchor.position.y
+	anchor_absolute_x = DialogAnchor.position.x
+	
+	_flip()
+
 func _process(delta):
 	time_elapsed = wrapf(time_elapsed + delta, 0, period)
 	offset.y = amplitude * sin((time_elapsed / period) * 2 * PI)
+	$DialogAnchor.position.y = original_anchor_y + offset.y
+
+func _flip():
+	var scale = -1 if flip_h else 1
+	
+	$DialogAnchor.position.x = scale * anchor_absolute_x
